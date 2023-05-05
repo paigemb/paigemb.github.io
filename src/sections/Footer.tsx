22ceab7257d406ef0942a64fe8f7038c40411134
paigemb <66384191+paigemb@users.noreply.github.com>
2023-05-05 09:12:57 -0700

    remove import

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Container } from 'react-bootstrap'
-import about from '../data/about.json'
 
 const StyledFooter = styled.footer({
   marginTop: '110px'
